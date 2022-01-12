3ca31ec5e2627cc9ae021b8be073a6eb2745ddd7
Marvin Drees <marvin.drees@9elements.com>
2022-01-12 12:22:05 +0100

    go mod init
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/system-transparency/stmgr
+
+go 1.17
+
+require github.com/alecthomas/kong v0.3.0
+
+require github.com/pkg/errors v0.9.1 // indirect
---
+++ stmgr.go
@@ -0,0 +1,14 @@
+// Copyright 2022 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "github.com/alecthomas/kong"
+
+var CLI struct {
+}
+
+func main() {
+	kong.Parse(&CLI)
+}
