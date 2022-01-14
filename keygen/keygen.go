fc3e00f0e43b4615fbe2d1edfa5ec8c036154cec
Marvin Drees <marvin.drees@9elements.com>
2022-01-14 14:19:23 +0100

    Add keygen command from old stmanager to stmgr
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

---
+++ keygen/keygen.go
@@ -0,0 +1,208 @@
+// Copyright 2022 the System Transparency Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package keygen
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	DefaultCertName     = "cert.pem"
+	DefaultRootCertName = "rootcert.pem"
+	DefaultKeyName      = "key.pem"
+	DefaultRootKeyName  = "rootkey.pem"
+)
+
+func Run(isCa bool, rootCertPath, rootKeyPath, validFrom, validUntil, certOut, keyOut string) error {
+	keyOut, err := parseKeyPath(isCa, keyOut)
+	if err != nil {
+		return err
+	}
+
+	certOut, err = parseCertPath(isCa, certOut)
+	if err != nil {
+		return err
+	}
+
+	notBefore, err := parseValidFrom(validFrom)
+	if err != nil {
+		return err
+	}
+
+	notAfter, err := parseValidUntil(validUntil)
+	if err != nil {
+		return err
+	}
+
+	var newCert *x509.Certificate
+	var newKey ed25519.PrivateKey
+	if rootCertPath == "" || rootKeyPath == "" {
+		newCert, newKey, err = newCertWithED25519Key(nil, nil, notBefore, notAfter)
+		if err != nil {
+			return err
+		}
+	} else {
+		rootCertBlock, err := loadPEM(rootKeyPath)
+		if err != nil {
+			return err
+		}
+		rootCert, err := x509.ParseCertificate(rootCertBlock.Bytes)
+		if err != nil {
+			return err
+		}
+
+		rootKeyBlock, err := loadPEM(rootKeyPath)
+		if err != nil {
+			return err
+		}
+		rootKey, err := x509.ParsePKCS8PrivateKey(rootKeyBlock.Bytes)
+		if err != nil {
+			return err
+		}
+
+		newCert, newKey, err = newCertWithED25519Key(rootCert, &rootKey, notBefore, notAfter)
+		if err != nil {
+			return err
+		}
+	}
+
+	key, err := x509.MarshalPKCS8PrivateKey(newKey)
+	if err != nil {
+		return err
+	}
+
+	certBlock := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: newCert.Raw,
+	}
+	if err := writePEM(&certBlock, certOut); err != nil {
+		return err
+	}
+
+	keyBlock := pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: key,
+	}
+	if err := writePEM(&keyBlock, keyOut); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseKeyPath(isCA bool, path string) (string, error) {
+	if path == "" {
+		if isCA {
+			return DefaultRootKeyName, nil
+		}
+		return DefaultKeyName, nil
+	}
+	dir := filepath.Dir(path)
+	if _, err := os.Stat(dir); err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func parseCertPath(isCA bool, path string) (string, error) {
+	if path == "" {
+		if isCA {
+			return DefaultRootCertName, nil
+		}
+		return DefaultCertName, nil
+	}
+	dir := filepath.Dir(path)
+	if _, err := os.Stat(dir); err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func parseValidFrom(date string) (time.Time, error) {
+	if len(date) == 0 {
+		return time.Now(), nil
+	}
+	return time.Parse(time.RFC822, date)
+}
+
+func parseValidUntil(date string) (time.Time, error) {
+	if len(date) == 0 {
+		return time.Now().Add(72 * time.Hour), nil
+	}
+	return time.Parse(time.RFC822, date)
+}
+
+func newCertWithED25519Key(rootCert *x509.Certificate, rootKey *interface{}, notBefore, notAfter time.Time) (*x509.Certificate, ed25519.PrivateKey, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+	}
+
+	newPub, newPriv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var certBytes []byte
+	if rootCert == nil || rootKey == nil {
+		template.KeyUsage |= x509.KeyUsageCertSign
+		template.BasicConstraintsValid = true
+		template.IsCA = true
+		certBytes, err = x509.CreateCertificate(rand.Reader, &template, &template, newPub, newPriv)
+		if err != nil {
+			return nil, nil, err
+		}
+	} else {
+		certBytes, err = x509.CreateCertificate(rand.Reader, &template, rootCert, newPub, newPriv)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	newCert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return newCert, newPriv, nil
+}
+
+func loadPEM(path string) (*pem.Block, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	block, rest := pem.Decode(bytes)
+	if block == nil {
+		return nil, errors.New("no PEM block found")
+	}
+	if len(rest) != 0 {
+		return nil, errors.New("unexpected trailing data after PEM block")
+	}
+
+	return block, nil
+}
+
+func writePEM(block *pem.Block, path string) error {
+	pemBytes := pem.EncodeToMemory(block)
+	return os.WriteFile(path, pemBytes, 0666)
+}
--- stmgr.go
+++ stmgr.go
@@ -9,16 +9,22 @@ import (
 	"log"
 	"os"
 
+	"github.com/system-transparency/stmgr/keygen"
 	"github.com/system-transparency/stmgr/provision"
 )
 
-var helpText = `
-Usage:
+const helpText = `
+Usage: stmgr <COMMAND> [subcommands...]
 	provision:
 		Allows creating host configurations by spawning a TUI in
 		which the user can input values into that are converted
 		into a host_configuration.json file.
-			-efi: Store the output in the efivarfs
+
+	keygen:
+		Generate certificates for signing OS packages
+		using ED25519 keys.
+
+Use stmgr <COMMAND> -help for more info.
 `
 
 func main() {
@@ -33,20 +39,35 @@ func main() {
 func run(args []string) error {
 	// Display helptext if no arguments are given
 	if len(args) < 2 {
-		log.Println(helpText)
+		log.Print(helpText)
 		return nil
 	}
 
-	// Provision tool and subcommands
-	provisionCmds := flag.NewFlagSet("provision", flag.ContinueOnError)
-	efi := provisionCmds.Bool("efi", false, "Store host_configuration.json in the efivarfs")
-
+	// Evaluate the cli arguments
 	switch args[1] {
 	case "provision":
-		provisionCmds.Parse(args[2:])
+		// Provision tool and subcommands
+		provisionCmd := flag.NewFlagSet("provision", flag.ContinueOnError)
+		efi := provisionCmd.Bool("efi", false, "Store host_configuration.json in the efivarfs")
+
+		provisionCmd.Parse(args[2:])
 		return provision.Run(*efi)
+	case "keygen":
+		// Keygen tool and subcommands
+		keygenCmd := flag.NewFlagSet("keygen", flag.ContinueOnError)
+		keygenRootCert := keygenCmd.String("rootCert", "", "Root certificate in PEM format to sign the new certificate. Ignored if -isCA is set")
+		keygenRootKey := keygenCmd.String("rootKey", "", "Root key in PEM format to sign the new certificate. Ignored if -isCA is set")
+		keygenIsCA := keygenCmd.Bool("isCA", false, "Generate self signed root certificate")
+		keygenValidFrom := keygenCmd.String("validFrom", "", "Date formatted as RFC822. Defaults to time of creation.")
+		keygenValidUntil := keygenCmd.String("validUntil", "", "Date formatted as RFC822. Defaults to time of creation + 72h")
+		keygenCertOut := keygenCmd.String("certOut", "", "Output certificate file. Defaults to cert.pem or rootcert.pem is -isCA is set.")
+		keygenKeyOut := keygenCmd.String("keyOut", "", "Output key file. Defaults to key.pem or rootkey.pem if -isCA is set.")
+
+		keygenCmd.Parse(args[2:])
+		return keygen.Run(*keygenIsCA, *keygenRootCert, *keygenRootKey, *keygenValidFrom, *keygenValidUntil, *keygenCertOut, *keygenKeyOut)
 	default:
-		log.Println(helpText)
+		// Display helptext on unknown command
+		log.Print(helpText)
 		return nil
 	}
 }
